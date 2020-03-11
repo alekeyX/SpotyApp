5a2238e2591fc64631c625def62dc6811fc4e3cd
Alejandro Rojas <alejandrod.rojasg@gmail.com>
2020-03-11 00:20:20 -0400

    anadido rutas y modulo http, servicio spotify, y navbar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  paises: any[] = [];
 
-  ngOnInit(): void {
+  constructor ( private spotify: SpotifyService ) {
+    this.spotify.getNewReleases();
   }
+
 
 }
