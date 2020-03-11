c5d26f48a70e34c0cfe514abaf97bc742663c8df
Alejandro Rojas <alejandrod.rojasg@gmail.com>
2020-03-11 11:34:21 -0400

    actualizacion de servicios de spotify, aumento de noimage.png

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,33 @@ export class SpotifyService {
     console.log('Spotify listo');
   }
 
-  getNewReleases() {
-    // para mandar el token de cabecera
+  getQuery( query: string ) {
+    const url = `https://api.spotify.com/v1/${ query }`;
+
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQB2ZNTUpbkDnKoa2yfcpi26lJRpTLTDBXujWQ8DRxoW7bDy4Q9PazNF5me49jSq261vtv9MPlO5IZYjwpI'
+      'Authorization': 'Bearer BQBBpfmBGaUHLeecEUm82naBEZMR2UKmjTt3I0FddT36xTsqsCpWGQ47T6bzZyVRmx2aMCLzuvqy0-S7n8U'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
+    // para mandar el token de cabecera
+    // const headers = new HttpHeaders({
+    //   'Authorization': 'Bearer BQBBpfmBGaUHLeecEUm82naBEZMR2UKmjTt3I0FddT36xTsqsCpWGQ47T6bzZyVRmx2aMCLzuvqy0-S7n8U'
+    // });
+    // this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
+    //           .pipe( map( data => {
+    //             return data['albums'].items;
+    //           }))
+
+    return this.getQuery('browse/new-releases')
+              .pipe( map( data => data['albums'].items ));
+  }
+
+  getArtista( termino: string ){
+
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+              .pipe( map( data => data['artists'].items));
   }
 }
